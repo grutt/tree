2690983ba22af4856e60231f538a2cb0cc6d1a07
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-01 21:43:05 +0000

    codeclimate cyclo-comp fix

--- pytree.py
+++ pytree.py
@@ -3,6 +3,25 @@ import os
 import argparse
 import string
 
+# our printy blocks
+VERTI_LINE = "│   "
+CONT_BRANCH = "├── "
+END_BRANCH = "└── "
+BLOC_SPACE = "    "
+
+def removeHidden(dirContents):
+    dirTmp = list(dirContents)
+    dirContents = []
+    for key, thing in enumerate(dirTmp):
+        if not os.path.basename(thing).startswith('.'):
+            dirContents.append(thing)
+    return dirContents
+
+def makeLeftspace(colContinues):
+    leftSpace = ""
+    for col in colContinues:
+        leftSpace += VERTI_LINE if col else BLOC_SPACE
+    return leftSpace
 
 def treeify(path):
     print(path)
@@ -14,19 +33,12 @@ def treeify(path):
 
 
 def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden=True):
-    # our printy blocks
-    vertLine = "│   "
-    contBranch = "├── "
-    endBranch = "└── "
-    blockSpace = "    "
 
     # determine which lines we need
-    leftSpace = ""
-    for col in colContinues:
-        leftSpace += vertLine if col else blockSpace
+    leftSpace = makeLeftspace(colContinues)
 
     if depth > 0:
-        branch = contBranch if True else endBranch
+        branch = CONT_BRANCH if True else END_BRANCH
 
     # get contents
     dirContents = os.listdir(path)
@@ -34,11 +46,7 @@ def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden=True):
 
     # remove hidden files
     if no_hidden:
-        dirTmp = list(dirContents)
-        dirContents = []
-        for key, thing in enumerate(dirTmp):
-            if not os.path.basename(thing).startswith('.'):
-                dirContents.append(thing)
+        dirContents = removeHidden(dirContents)
 
     # http://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
     translation_table = dict.fromkeys(map(ord, string.punctuation), None)
@@ -50,7 +58,7 @@ def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden=True):
 
     # recurse and print
     for key, thing in enumerate(dirContents):
-        branch = contBranch if key < (len(dirContents)-1) else endBranch
+        branch = CONT_BRANCH if key < (len(dirContents) - 1) else END_BRANCH
 
         if os.path.isdir(os.path.join(path, thing)):
             cC = list(colContinues)
@@ -59,7 +67,7 @@ def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden=True):
             else:
                 cC.append(False)
 
-            print( leftSpace + branch + os.path.basename(thing))
+            print( leftSpace + branch + os.path.basename(thing) )
             d, f = treeify_helper(os.path.join(path, thing), depth + 1, cC, 1, 0)
             nDir += d
             nFil += f
