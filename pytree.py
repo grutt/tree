851ff1b313368b4c17fb9fa4c4e9e626d908cbdd
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-01 21:49:31 +0000

    codeclimate cyclo-comp fix

--- pytree.py
+++ pytree.py
@@ -9,6 +9,7 @@ CONT_BRANCH = "├── "
 END_BRANCH = "└── "
 BLOC_SPACE = "    "
 
+
 def removeHidden(dirContents):
     dirTmp = list(dirContents)
     dirContents = []
@@ -17,11 +18,25 @@ def removeHidden(dirContents):
             dirContents.append(thing)
     return dirContents
 
+
 def makeLeftspace(colContinues):
     leftSpace = ""
     for col in colContinues:
         leftSpace += VERTI_LINE if col else BLOC_SPACE
     return leftSpace
+
+
+def sortIgnoreCaseandPunc(dirContents):
+    # http://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
+    # http://stackoverflow.com/questions/9764298/is-it-possible-to-sort-two-listswhich-reference-each-other-in-the-exact-same-w
+    stdStrs = []
+
+    translation_table = dict.fromkeys(map(ord, string.punctuation), None)
+    for i in dirContents:
+        stdStrs.append(i.translate(translation_table).lower())
+    stdStrs, dirContents = (list(t) for t in zip(*sorted(zip(stdStrs, dirContents))))
+    return dirContents
+
 
 def treeify(path):
     print(path)
@@ -33,28 +48,17 @@ def treeify(path):
 
 
 def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden=True):
-
     # determine which lines we need
     leftSpace = makeLeftspace(colContinues)
 
-    if depth > 0:
-        branch = CONT_BRANCH if True else END_BRANCH
-
     # get contents
     dirContents = os.listdir(path)
-    stdStrs = []
 
     # remove hidden files
     if no_hidden:
         dirContents = removeHidden(dirContents)
 
-    # http://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
-    translation_table = dict.fromkeys(map(ord, string.punctuation), None)
-    for i in dirContents:
-        stdStrs.append(i.translate(translation_table).lower())
-
-    stdStrs, dirContents = (list(t) for t in zip(*sorted(zip(stdStrs, dirContents))))
-    # http://stackoverflow.com/questions/9764298/is-it-possible-to-sort-two-listswhich-reference-each-other-in-the-exact-same-w
+    dirContents = sortIgnoreCaseandPunc(dirContents)
 
     # recurse and print
     for key, thing in enumerate(dirContents):
@@ -67,7 +71,7 @@ def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden=True):
             else:
                 cC.append(False)
 
-            print( leftSpace + branch + os.path.basename(thing) )
+            print(leftSpace + branch + os.path.basename(thing))
             d, f = treeify_helper(os.path.join(path, thing), depth + 1, cC, 1, 0)
             nDir += d
             nFil += f
