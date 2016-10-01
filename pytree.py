1e1f10cf1a7a4acb57dc0ed778695f4e45d6d633
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-01 21:34:52 +0000

    codeclimate corrections

--- pytree.py
+++ pytree.py
@@ -3,7 +3,6 @@ import os
 import argparse
 import string
 
-# YOUR CODE GOES here
 
 def treeify(path):
     print(path)
@@ -13,25 +12,25 @@ def treeify(path):
 
     print("\n{} {}, {} {}".format(nDir, sDir, nFil, sFil))
 
-def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden = True):
+
+def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden=True):
     # our printy blocks
-    vertLine   = "│   "
+    vertLine = "│   "
     contBranch = "├── "
-    endBranch  = "└── "
+    endBranch = "└── "
     blockSpace = "    "
 
-    #determine which lines we need
+    # determine which lines we need
     leftSpace = ""
     for col in colContinues:
         leftSpace += vertLine if col else blockSpace
 
-    if depth>0:
-        branch =  contBranch if True else endBranch
+    if depth > 0:
+        branch = contBranch if True else endBranch
 
-    #get contents
-    dirContents =  os.listdir(path)
+    # get contents
+    dirContents = os.listdir(path)
     stdStrs = []
-
 
     # remove hidden files
     if no_hidden:
@@ -41,7 +40,7 @@ def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden = True):
             if not os.path.basename(thing).startswith('.'):
                 dirContents.append(thing)
 
-    ##http://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
+    # http://stackoverflow.com/questions/3939361/remove-specific-characters-from-a-string-in-python
     translation_table = dict.fromkeys(map(ord, string.punctuation), None)
     for i in dirContents:
         stdStrs.append(i.translate(translation_table).lower())
@@ -49,19 +48,19 @@ def treeify_helper(path, depth, colContinues, nDir, nFil, no_hidden = True):
     stdStrs, dirContents = (list(t) for t in zip(*sorted(zip(stdStrs, dirContents))))
     # http://stackoverflow.com/questions/9764298/is-it-possible-to-sort-two-listswhich-reference-each-other-in-the-exact-same-w
 
-    #recurse and print
+    # recurse and print
     for key, thing in enumerate(dirContents):
         branch = contBranch if key < (len(dirContents)-1) else endBranch
 
         if os.path.isdir(os.path.join(path, thing)):
             cC = list(colContinues)
-            if key < (len(dirContents)-1):
+            if key < (len(dirContents) - 1):
                 cC.append(True)
             else:
                 cC.append(False)
 
             print( leftSpace + branch + os.path.basename(thing))
-            d,f = treeify_helper(os.path.join(path, thing), depth+1, cC, 1, 0)
+            d, f = treeify_helper(os.path.join(path, thing), depth + 1, cC, 1, 0)
             nDir += d
             nFil += f
         else:
